08fd82ac167d855982073774e3e3f043cfe4ff28
Nekonium project <nyatla39+nekonium@gmail.com>
2018-02-02 10:50:28 +0900

    Create publicnode.js

---
+++ data/publicnode.js
@@ -0,0 +1,8 @@
+{
+"version":"publicnodes/0.1",
+"nodes":[
+    {"name":"ROZ#1","url":"http://nekonium.site:8293/"},
+    {"name":"ROZ#2","url":"https://www.nekonium.site:8293/"},
+    {"name":"ROZ#3","url":"https://ssl.nekonium.site:8293/"}
+  ]
+}
