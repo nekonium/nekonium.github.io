fbcda9181c67095662a99f8b472b409d6cbfa70a
Nekonium project <nyatla39+nekonium@gmail.com>
2018-02-04 15:41:12 +0900

    Update publicnode.js

--- data/publicnode.js
+++ data/publicnode.js
@@ -4,6 +4,6 @@
     {"name":"ROZ#1","url":"http://nekonium.site:8293/"},
     {"name":"ROZ#2","url":"https://www.nekonium.site:8293/"},
     {"name":"ROZ#3","url":"https://ssl.nekonium.site:8293/"},
-    {"name":"ohac#3","url":"https://nekonium.sighash.info:8293/"}
+    {"name":"ohac#1","url":"https://nekonium.sighash.info:8293/"}
   ]
 }
